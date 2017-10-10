5290b071afd4854ac1145c8fcf7e4d5c62d1206c
FIC <lucas_redigolo@hotmail.com>
2017-10-10 20:44:30 -0300

    aula sobre switch/case

---
+++ Switch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace aulasteste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o ultimo digito da sua placa:");
+
+            string Placa = Console.ReadLine();
+
+            switch(Placa){
+                case "1":
+                    System.Console.WriteLine("Segunda-Feira");
+                break;
+                case "2":
+                    System.Console.WriteLine("Terça-Feira");
+                break;
+                case "3":
+                    System.Console.WriteLine("Quarta-Feira");
+                break;
+                case "4":
+                    System.Console.WriteLine("Quinta-Feira");
+                break;
+                case "5":
+                    System.Console.WriteLine("Sexta-Feira");
+                break;
+
+            }
+        }
+    }
+}
