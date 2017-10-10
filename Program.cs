30e3f4af36ca805186471d88d4fb2145d6449f03
FIC <lucas_redigolo@hotmail.com>
2017-10-10 19:12:44 -0300

    testei algumas variaveis if

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace aulasteste
+{
+    class Program
+    
+    {
+        static void Main (string [] arg)
+
+        
+        {
+
+       double NotadoAluno = 8.5;
+
+       System.Console.WriteLine("Calculando Nota....");
+       System.Threading.Thread.Sleep(5000);
+  
+       if (NotadoAluno>7)
+
+        System.Console.WriteLine("Aluno Aprovado");
+        
+       else if (NotadoAluno <7 && NotadoAluno  >5)
+       
+       System.Console.WriteLine("Aluno Reprovado");     
+    
+
+        }
+    }
+}
